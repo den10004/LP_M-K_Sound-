ebceb0f341c5f66c393a5451685f06bcd0187971
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-02-03 16:21:09 +0300

    блок мониторы

--- js/sliders.js
+++ js/sliders.js
@@ -1,143 +1,30 @@
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper,
+var swiper = new Swiper(".sound__slider", {
+  slidesPerView: 5,
+  spaceBetween: 65,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    400: {
+      slidesPerView: 1.5,
+    },
+    425: {
+      slidesPerView: 2,
+    },
+    1300: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+    1401: {
+      slidesPerView: 5,
+      spaceBetween: 65,
+    },
   },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper3 = new Swiper(".mySwiper3", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper4 = new Swiper(".mySwiper4", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper3,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper5 = new Swiper(".mySwiper5", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper6 = new Swiper(".mySwiper6", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper5,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper7 = new Swiper(".mySwiper7", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper8 = new Swiper(".mySwiper8", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper7,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper9 = new Swiper(".mySwiper9", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper10 = new Swiper(".mySwiper10", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper9,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper11 = new Swiper(".mySwiper11", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper12 = new Swiper(".mySwiper12", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper11,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper13 = new Swiper(".mySwiper13", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper14 = new Swiper(".mySwiper14", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper13,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper15 = new Swiper(".mySwiper15", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper16 = new Swiper(".mySwiper16", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper15,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper17 = new Swiper(".mySwiper17", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper18 = new Swiper(".mySwiper18", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper17,
-  },
-  pagination: {
-    el: ".swiper-pagination",
+  navigation: {
+    nextEl: ".cardsSlider-button-prev",
+    prevEl: ".cardsSlider-button-next",
   },
 });
